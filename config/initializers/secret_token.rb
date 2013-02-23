f8298b9c7c54c1540c20b546bfd4cc2366c4333f
QuanYuan <quanyuan@seas.upenn.edu>
2013-02-23 16:46:39 -0500

    hw1 for cis196

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw1::Application.config.secret_token = '501f8a5d8e948b9246591852b03cbfa7bc2fcbc3767370c71a07ca37f80464cc9842db47285b6911557818e9928dcbf132aeadf281c2c6599e458ccb7e8c521a'
